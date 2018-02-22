7dcc4006905730f4270309ab42eec1c557dc0b2c
Anne Fraysse <afraysse@tetrascience.com>
2018-02-22 18:00:52 -0500

    added in form components

--- containers/App.js
+++ containers/App.js
@@ -1,50 +1,73 @@
-import React from 'react'
-import { Container, Grid, Header, Divider } from 'semantic-ui-react'
+import React, {Component} from 'react'
+import { Container, Button, Checkbox, Form, Header, Divider } from 'semantic-ui-react'
 
-const ThemingLayout = () => (
-  <Container style={{ marginTop: '3em '}}>
-  <Header as='h1' dividing>Bug Reporting Form</Header>
+const projectOptions = [
+  {key: 'sched', text: 'Scheduling', value: 'Scheduling'},
+  {key: 'lm', text: 'Lab Monitoring', value: 'Lab Monitoring'},
+  {key: 'ur', text: 'Utilization Reporting', value: 'Utilization Reporting'},
+  {key: 'dg', text: 'Device Groups', value: 'Device Groups'},
+  {key: 'reg', text: 'User Registration', value: 'User Registration'}
+]
 
-  <Grid columns={3} stackable>
-    <Grid.Column>
-      <Header as='h1'>My Name is Annie</Header>
-      <Header as='h4'>Fraysse</Header>
+const priorityRating = [
+  {key: 'vl', text: 'Very Low', value: 'Very Low'},
+  {key: 'l', text: 'Low', value: 'Low'},
+  {key: 'm', text: 'Medium', value: 'Medium'},
+  {key: 'h', text: 'High', value: 'High'},
+  {key: 'c', text: 'Critical', value: 'Critical'}
+]
 
-    <p>
-    I was born in sunny California on a cool January afternoon.
-    </p>
-    </Grid.Column>
+const webBrowser = [
+  {key: 'ie11', text: 'IE 11', value: 'IE11'},
+  {key: 'chrome', text: 'Chrome', value: 'chrome'},
+  {key: 'firefox', text: 'Firefox', value: 'firefox'},
+  {key: 'safari', text: 'Safari', value: 'safari'}
+]
 
-    <Grid.Column>
-      <Header as='h2'>Lets all catch bugs!</Header>
+const opSys = [
+  {key: 'Mac OS', text: 'Mac OS', value: 'Mac OS'},
+  {key: 'Windows', text: 'Windows', value: 'Windows'}
+]
 
-      <p>
-      Bugs are no longer just a nusiance; they are a way of life. Or so you might hear a QA say.
-      </p>
-      <p>
-        <small>Bugs can be very very small!</small>
-      </p>
-      <p>Bugs can also be <strong>very bold!</strong>.</p>
-      <p>Bugs can be <em>sneaky</em>!</p>
-      <p>Bugs can also not be <abbr title='bugs'>bgs</abbr> at all!</p>
-    </Grid.Column>
+class bugFormatLayout extends Component {
+  state = {}
+  handleChange = (e, { value }) => this.setState({value})
 
-    <Grid.Column>
-      <Grid
-        centered
-        columns={3}
-        padded
-        stackable
-        style={{ margin: '-1.5em', width: 400 }}
-        textAlign='middle'
-      >
-      <Grid.Column color='red' style={{ margin: '0.5em', height: 50 }}>Red</Grid.Column>
-      <Grid.Column color='orange' style={{ margin: '0.5em', height: 50 }}>Orange</Grid.Column>
-      <Grid.Column color='yellow' style={{ margin: '0.5em', height: 50 }}>Yellow</Grid.Column>
-      </Grid>
-    </Grid.Column>
-  </Grid>
-</Container>
-)
+  render() {
+    const {value} = this.state
+    return (
+    <Container style={{ marginTop: '3em '}}>
+    <Header as='h1' dividing>TetraScience Bug Reporting Form</Header>
+    <Form>
+      <Form.Group widths='equal'>
+        <Form.Input fluid label='Internal Reporter' placeholder='Internal Name' />
+        <Form.Input fluid label='External Reporter' placeholder='External Name' />
+        <Form.Input fluid label='External Reporter Company' placeholder='Company Name' />
+      </Form.Group>
+      <Form.Group widths='equal'>
+        <Form.Select fluid label='Product' options={projectOptions} placeholder='Scheduling' />
+        <Form.Select fluid label='Priority' options={priorityRating} placeholder='Priority' />
+      </Form.Group>
+      <Form.Group widths='equal'>
+        <Form.Select fluid label='Web Browser' options={webBrowser} placeholder='Web Browser' />
+        <Form.Select fluid label='Operating System' options={opSys} placeholder='Operating System' />
+      </Form.Group>
+      <Form.Group inline>
+        <label>Suggested Fix Time</label>
+        <Form.Radio label='Today' value='today' checked={value === 'today'} onChage={this.handleChange} />
+        <Form.Radio label='Current Sprint' value='current_sprint' checked={value === 'current_sprint'} onChage={this.handleChange} />
+        <Form.Radio label='1 - 2 Weeks' value='1-2' checked={value === '1-2'} onChange={this.handleChange} />
+        <Form.Radio label='1 Month' value='1-m' checked={value === '1-m'} onChange={this.handleChange} />
+        <Form.Radio label='Time Permits' value='time_permits' checked={value === 'time_permits'} onChange={this.handleChange} />
+      </Form.Group>
+      <Form.Input fluid label='Bug Title' placeholder='Unable to log into...' />
+      <Form.TextArea label="Bug Description" placeholder="Describe what happened." />
+    </Form>
 
-export default ThemingLayout
+
+    </Container>
+    )
+  }
+}
+
+export default bugFormatLayout
--- index.js
+++ index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 
 // main app
 import App from './containers/App';
+import dashboard from './containers/dashboard';
 
-ReactDOM.render(<App />, document.getElementById('app')) 
+ReactDOM.render(<App />, document.getElementById('app'))
