54bebf3a5b7c6a9a0b1d33c8d10b0af81b5385a2
Anne Fraysse <afraysse@tetrascience.com>
2018-02-27 13:46:49 -0500

    reformatting browser

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -7,10 +7,10 @@ module.exports = {
       loaders: [{
         test: /\.(js|jsx)$/,
             exclude: /node_modules/,
-            loader: 'babel-loader'
+            use: 'babel-loader'
       },{
             test: /\.less$/,
-            loaders: ["style-loader", "css-loader", "less-loader"]
+            use: ["style-loader", "css-loader", "less-loader"]
       }
     ]
   },
